1a9ea1b432233341ed986337f0e9656a3a995076
Sadorra <rodiawg@gmail.com>
2025-11-15 08:50:24 +0300

    Fix: fixed requirement fail.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,23 @@
 
 from django.urls import path
-from .views import LoginView, LogoutView, register
-from .views import list_books, LibraryDetailView   # keep previous imports
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # --- Authentication ---
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    path(
+        'login/',
+        LoginView.as_view(template_name='relationship_app/login.html'),
+        name='login'
+    ),
+    path(
+        'logout/',
+        LogoutView.as_view(template_name='relationship_app/logout.html'),
+        name='logout'
+    ),
+    path('register/', views.register, name='register'),
 ]
+
