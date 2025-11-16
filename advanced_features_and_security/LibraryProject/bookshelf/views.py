dd5510b5c919046572127cb5bcf38922a8c6ae1c
Sadorra <rodiawg@gmail.com>
2025-11-16 22:51:53 +0300

    Fix: fixed requirement fail

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
+from .models import Book
+
+
+
+def book_list(request):
+    books = Book.objects.all()      
+    return render(request, "book_list.html", {"books": books})
+
+def books(request):
+    all_books = Book.objects.all()
+    return render(request, "books.html", {"books": all_books})
+
 
 @permission_required('accounts.can_view', raise_exception=True)
 def view_post(request):
