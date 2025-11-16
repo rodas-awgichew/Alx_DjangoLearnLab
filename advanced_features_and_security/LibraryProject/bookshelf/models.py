b7da6e68310a7b419422c44e43fa5450db5b4a26
Sadorra <rodiawg@gmail.com>
2025-11-16 22:43:53 +0300

    Fix: fixed requirement fail.

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -34,6 +34,15 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
