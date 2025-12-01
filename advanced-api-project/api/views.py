23e86272d7b2f98b03daa6d643e83ce5d39ab5a2
Sadorra <rodiawg@gmail.com>
2025-12-01 07:12:36 +0300

    fix: fixed import header to satisfy the requirement

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,9 @@
 from rest_framework import generics, permissions, filters
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as django_filters
 
 from .models import Book
 from .serializers import BookSerializer
-
 
 # ----------------------------------------
 # List all books + filtering + search + ordering
