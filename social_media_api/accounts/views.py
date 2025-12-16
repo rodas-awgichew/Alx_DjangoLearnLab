70aa52d4159e4d7eee50ab365beb82c8d61b540c
Sadorra <rodiawg@gmail.com>
2025-12-16 21:38:34 +0300

    feat: Configure user authentication, define URL patterns and applied testing and initial Launch

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token = Token.objects.get(user=user)
+            return Response({
+                'token': token.key,
+                'message': 'User registered successfully'
+            })
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        user = authenticate(
+            username=request.data.get('username'),
+            password=request.data.get('password')
+        )
+
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        return Response(
+            {'error': 'Invalid credentials'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+class ProfileView(APIView):
+    def get(self, request):
+        user = request.user
+        return Response({
+            'username': user.username,
+            'email': user.email,
+            'bio': user.bio,
+            'followers': user.followers.count(),
+            'following': user.following.count(),
+        })
