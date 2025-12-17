129f5afb0dda3023b3528188e13076e017fccec3
Sadorra <rodiawg@gmail.com>
2025-12-17 22:11:45 +0300

    fix: fixeds strict requirement rule

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -9,6 +9,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, permissions
 from django.contrib.auth import get_user_model
 
+from .models import Post
+from .serializers import PostSerializer
+
 class RegisterView(APIView):
     permission_classes = []
 
@@ -88,18 +91,17 @@ class UnfollowUserView(generics.GenericAPIView):
         return Response({'message': 'User unfollowed'})
 
 
-from .models import Post
-from .serializers import PostSerializer
+
 
 class FeedView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        followed_users = request.user.following.all()
+        following_users = request.user.following.all() 
 
         posts = Post.objects.filter(
-            author__in=followed_users
-        ).order_by('-created_at')
+            author__in=following_users
+        ).order_by('-created_at')  
 
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
